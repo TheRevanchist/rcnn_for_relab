5cd995814265cf7f8775db04b82f41b06acfba9d
aretor <torci@live.it>
2017-09-18 15:47:38 +0200

    code rewriting
    Some line of code has been rewritten to obtain more elegant and
    optimized code.

--- input_relab.py
+++ input_relab.py
@@ -11,14 +11,15 @@ import pickle
 from numpy import unravel_index
 from test2 import *
 
+home = os.path.expanduser('~')
+repo_of_ground_truth = home + '/VOCdevkit/VOC2007/Annotations'
+repo_of_images = home + '/VOCdevkit/VOC2007/JPEGImages'
+train_set = home + '/VOCdevkit/VOC2007/ImageSets/Main/test.txt'
 
-repo_of_ground_truth = '/home/revan/VOCdevkit/VOC2007/Annotations'
-repo_of_images = '/home/revan/VOCdevkit/VOC2007/JPEGImages'
-train_set = '/home/revan/VOCdevkit/VOC2007/ImageSets/Main/test.txt'
-
-classes_dict = {'__background__': 0, 'aeroplane': 1, 'bicycle': 2, 'bird': 3, 'boat': 4, 'bottle': 5, 'bus': 6, 'car': 7,
-           'cat': 8, 'chair': 9, 'cow': 10, 'diningtable': 11, 'dog': 12, 'horse': 13, 'motorbike': 14,
-           'person': 15, 'pottedplant': 16, 'sheep': 17, 'sofa': 18, 'train': 19, 'tvmonitor': 20}
+classes_dict = {'__background__': 0, 'aeroplane': 1, 'bicycle': 2, 'bird': 3, 'boat': 4, 'bottle': 5, 'bus': 6,
+                'car': 7,
+                'cat': 8, 'chair': 9, 'cow': 10, 'diningtable': 11, 'dog': 12, 'horse': 13, 'motorbike': 14,
+                'person': 15, 'pottedplant': 16, 'sheep': 17, 'sofa': 18, 'train': 19, 'tvmonitor': 20}
 
 imdb_name = 'voc_2007_test'
 cfg_file = 'experiments/cfgs/faster_rcnn_end2end.yml'
@@ -33,7 +34,13 @@ vis = False
 def main():
     content = get_filenames_of_training_set(train_set)
     print(len(content))
-    p, gt, all_width_and_height = create_p_and_gt(content, repo_of_images, classes_dict)
+    # p, gt, all_width_and_height
+    all_data = create_p_and_gt(content, repo_of_images, classes_dict)
+    for var, fname in zip(all_data, ('p_test', 'gt_test', 'width_and_height', 'i_over_u_test')):
+        with open(fname + '.pickle', 'wb') as f:
+            # pickle <fname> data structure using the highest protocol available
+            pickle.dump(var, f, pickle.HIGHEST_PROTOCOL)
+
     with open('p_test.pickle', 'wb') as f:
         # Pickle p data structure using the highest protocol available.
         pickle.dump(p, f, pickle.HIGHEST_PROTOCOL)
@@ -80,15 +87,15 @@ def find_objects_in_files(filename, dict):
     :param element: the name of the image
     :return: points - a two 2 array containing the centers of each bounding box
     """
+
     tree = ET.parse(filename)
     root = tree.getroot()
 
     gt_info = []
 
     for size in root.iter('size'):
-        for width in size.iter('width'):
+        for width, height in zip(size.iter('width'), size.iter('height')):
             width = float(int(width.text))
-        for height in size.iter('height'):
             height = float(int(height.text))
 
     for object in root.iter('object'):
@@ -99,22 +106,12 @@ def find_objects_in_files(filename, dict):
                 name_decrypted = dict[class_of_object]
                 gt_info_object[name_decrypted] = 1.0
 
-        for bounding_box in object.iter('bndbox'):
-            for xmin in bounding_box.iter('xmin'):
-                # we convert first to float and then to int because for some reasons the bounding boxes can have float
-                # coordinates like 273.3863
-                xmin = int(float(xmin.text)) # / width
-            for ymin in bounding_box.iter('ymin'):
-                ymin = int(float(ymin.text)) # / height
-            for xmax in bounding_box.iter('xmax'):
-                xmax = int(float(xmax.text)) # / width
-            for ymax in bounding_box.iter('ymax'):
-                ymax = int(float(ymax.text)) # / height
-            # fill the spatial positions for each object
-            gt_info_object[21] = xmin
-            gt_info_object[22] = ymin
-            gt_info_object[23] = xmax
-            gt_info_object[24] = ymax
+        for bbox in object.iter('bndbox'):
+            # we convert first to float and then to int because for some reasons the bounding boxes can have float
+            # coordinates like 273.3863
+            for coo, i in (('xmin', 21), ('ymin', 22), ('xmax', 23), ('ymax', 24)):
+                gt_info_object[i] = int(float(bbox.find(coo).text))
+
         gt_info.append(gt_info_object)
     gt_info = np.asarray(gt_info)
     return gt_info, width, height
@@ -134,7 +131,7 @@ def test_net(name, net, imdb, im, max_per_image=300, thresh=0.3, vis=False):
         cls_scores = scores[inds, j]
         cls_boxes = boxes[inds, j * 4:(j + 1) * 4]
         cls_dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])) \
-                .astype(np.float32, copy=False)
+            .astype(np.float32, copy=False)
         keep = nms(cls_dets, cfg.TEST.NMS)
         cls_dets = cls_dets[keep, :]
         if cls_dets.shape[0] != 0:
@@ -152,17 +149,15 @@ def test_net(name, net, imdb, im, max_per_image=300, thresh=0.3, vis=False):
 
 
 def create_p_and_gt(content, repo_of_images, dict):
-
     rcnn_output = []
     ground_truth = []
     all_width_and_height = []
-
 
     for image_name in content:
         im = cv2.imread(os.path.join(repo_of_images, image_name + ".jpg"))
         xml_file = os.path.join(repo_of_ground_truth, image_name + ".xml")
 
-        all_scores, all_class_boxes = test_net(save_name, net, imdb, im, max_per_image, thresh=thresh, vis=0)
+        all_scores, all_class_boxes = test_net(save_name, net, imdb, im, max_per_image, thresh=thresh, vis=False)
 
         gt_info, width, height = find_objects_in_files(xml_file, dict)
         ground_truth.append(gt_info)
@@ -172,10 +167,10 @@ def create_p_and_gt(content, repo_of_images, dict):
             rcnn_output_individual[:, :21] = all_scores
             rcnn_output_individual[:, 21:] = all_class_boxes[:, :-1]
             rcnn_output.append(rcnn_output_individual)
-            all_width_and_height.append(np.asarray([width, height]))
         else:
-            rcnn_output.append(np.asarray([]))  # we add an empty array just to have everything synchronized
-            all_width_and_height.append(np.asarray([width, height]))
+            rcnn_output.append(np.empty((0, 0)))  # we add an empty array just to have everything synchronized
+
+        all_width_and_height.append(np.asarray([width, height]))
 
     return rcnn_output, ground_truth, all_width_and_height
 
@@ -204,28 +199,24 @@ def bb_intersection_over_union(boxA, boxB):
     return iou
 
 
-def postprocess(p, gt, all_width_and_height):
-    number_of_images = len(p)
-
+def postprocess(ps, gts, all_width_and_height):
     all_i_over_u = []
 
-    for i in xrange(number_of_images):
-        image_p = p[i]
-        image_gt = gt[i]
-        gt_len = len(image_gt)
-        p_len = len(image_p)
+    for p, gt in zip(ps, gts):
+        gt_len = len(gt)
+        p_len = len(p)
         i_over_u = np.zeros((gt_len, p_len))
         for j in xrange(gt_len):
             for k in xrange(p_len):
-                _intersection_over_union = bb_intersection_over_union(image_p[k, -4:], image_gt[j, -4:])
+                _intersection_over_union = bb_intersection_over_union(p[k, -4:], gt[j, -4:])
                 i_over_u[j, k] = _intersection_over_union
         all_i_over_u.append(i_over_u)
 
-
     all_p_and_gt_postprocessed = []
-    for i in xrange(number_of_images):
-        p_and_gt_postprocessed = find_best_matches(all_i_over_u[i], p[i], gt[i])
-        p_and_gt_postprocessed.append(all_width_and_height[i])
+
+    for aiou, p, gt, awah in zip(all_i_over_u, ps, gts, all_width_and_height):
+        p_and_gt_postprocessed = find_best_matches(aiou, p, gt)
+        p_and_gt_postprocessed.append(awah)
         all_p_and_gt_postprocessed.append(p_and_gt_postprocessed)
     with open('p_and_gt_test.pickle', 'wb') as f:
         # Pickle p data structure using the highest protocol available.
@@ -234,15 +225,14 @@ def postprocess(p, gt, all_width_and_height):
 
 
 def find_best_matches(numpy_array, p_object, gt_object):
-    rows = numpy_array.shape[0]
-    columns = numpy_array.shape[1]
+    rows, columns = numpy_array.shape
     max_number_matches = min(rows, columns)
     p_object_postprocessed = []
     gt_object_postprocessed = []
     rectangles_p = []
     rectangles_gt = []
     p_and_gt_postprocessed = []
-    for i in xrange(max_number_matches):
+    for _ in xrange(max_number_matches):
         best_match = unravel_index(numpy_array.argmax(), numpy_array.shape)
         # consider only non-zero intersection over union
         if numpy_array[best_match[0], best_match[1]]:
@@ -254,10 +244,10 @@ def find_best_matches(numpy_array, p_object, gt_object):
             gt_object_postprocessed.append(gt_object[best_match[0], :21])
             rectangles_gt.append(gt_object[best_match[0], 21:])
 
-    p_object_postprocessed = np.asarray(p_object_postprocessed)
-    gt_object_postprocessed = np.asarray(gt_object_postprocessed)
-    rectangles_p = np.asarray(rectangles_p)
-    rectangles_gt = np.asarray(rectangles_gt)
+    p_object_postprocessed = np.array(p_object_postprocessed)
+    gt_object_postprocessed = np.array(gt_object_postprocessed)
+    rectangles_p = np.array(rectangles_p)
+    rectangles_gt = np.array(rectangles_gt)
     p_and_gt_postprocessed.append(p_object_postprocessed)
     p_and_gt_postprocessed.append(gt_object_postprocessed)
     p_and_gt_postprocessed.append(rectangles_p)
@@ -279,6 +269,3 @@ if __name__ == "__main__":
     net.eval()
     main()
     print("meh")
-
-
-
